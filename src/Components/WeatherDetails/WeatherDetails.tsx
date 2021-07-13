657b8063ab6aa38e4a188b63d23ac8c376e2ccb9
2001-otter <nicolastheodarus@gmail.com>
2021-07-13 22:24:46 +0200

    editing card wrapper

--- src/Components/WeatherDetails/WeatherDetails.tsx
+++ src/Components/WeatherDetails/WeatherDetails.tsx
@@ -1,13 +1,22 @@
+import './weatherDetails.scss';
+
 import picture from '../../assets/images/Night-rainy.svg';
 
 const WeatherDetails = (props: any) => {
   return (
     <div className='weatherDetailsWrapper'>
-      <img
-        src={require('../../assets/images/Night-rainy.svg').default}
-        // src={import test from '../'}
-        alt='weather illustration'
-      ></img>
+      <div className='weatherIconWrapper'>
+        <img
+          src={require('../../assets/images/Night-rainy.svg').default}
+          // src={import test from '../'}
+          alt='weather icon'
+        ></img>
+      </div>
+      <div className='weatherDataWrapper'>
+        <p>25</p>
+        <p>clear</p>
+        <p>sunday</p>
+      </div>
     </div>
   );
 };
