5830ed32cebb4d2f1f9847b8e2392d21d888d3ea
2001-otter <nicolastheodarus@gmail.com>
2021-07-13 17:38:26 +0200

    set folder structure and assets

---
+++ src/Containers/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='App'>Weather APp</div>;
+    </div>
+  );
+}
+
+export default App;
